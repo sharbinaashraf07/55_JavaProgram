556f1e311cdf3b2af4c0518b1687347370e99c84
sharbinaashraf07 <sharbinaashraf1999@gmail.com>
2021-05-26 12:27:56 +0530

    javafile

---
+++ First15Programs/areaofcircle.java
@@ -0,0 +1,11 @@
+public class areaofcircle
+{
+public static void main(String args[])
+{
+double radius=7.5;
+double area=3.14*(radius*radius);
+System.out.println("area of circle="+area);
+double perimeter=2*3.14*radius;
+System.out.println("perimeter of circle="+perimeter);
+}
+}
