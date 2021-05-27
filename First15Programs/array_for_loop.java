752498f608626ddabfc8207ef1bd43887da5c53e
sharbinaashraf07 <sharbinaashraf1999@gmail.com>
2021-05-27 12:01:33 +0530

    javafile

---
+++ First15Programs/array_for_loop.java
@@ -0,0 +1,9 @@
+public class arrayele{
+public static void main(String args[]){
+String[] dis={"kannur","kozhikode","wayanad","idukki"};
+for(int i=0;i<dis.length;i++)
+{
+System.out.println(dis[i]);
+}
+}
+}
