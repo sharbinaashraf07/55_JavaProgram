ad881fb77cff245ca7af1e8c95136b114c43c37c
sharbinaashraf07 <sharbinaashraf1999@gmail.com>
2021-05-27 11:29:13 +0530

    javafile

---
+++ First15Programs/positive_or_negative.java
@@ -0,0 +1,13 @@
+public class positive{
+public static void main(String args[]){
+int a=10;
+if(a>0)
+{
+System.out.println(a+" is a positive number");
+}
+else if(a<0)
+{
+System.out.println(a+"is a negative numberS");
+}
+}
+}
