9750141a1d99a6fbda1341ec3ab12cece2cd3253
sharbinaashraf07 <sharbinaashraf1999@gmail.com>
2021-05-29 20:46:51 +0530

    javafile

---
+++ First15Programs/size.java
@@ -0,0 +1,17 @@
+public class size{
+public static void main(String args[]){
+int size=42;
+switch(size)
+{
+case 29:System.out.println("small");
+break;
+case 42:System.out.println("medium");
+break;
+case 44:System.out.println("large");
+break;
+case 48:System.out.println("extra large");
+break;
+default:System.out.println("unknown size");
+}
+}
+}
