0a3406c83b95e70c41e9bd386e09536740c7cc51
sharbinaashraf07 <sharbinaashraf1999@gmail.com>
2021-05-29 20:38:19 +0530

    javafile

---
+++ First15Programs/average.java
@@ -0,0 +1,10 @@
+public class average{
+public static void main(String args[]){
+int i,sum,avg;
+int a=1,b=2,c=3,d=4,e=5;
+sum=a+b+c+d+e;
+avg=sum/5;
+System.out.println("sum of numbers:"+sum);
+System.out.println("average of numbers:"+avg);
+}
+}
