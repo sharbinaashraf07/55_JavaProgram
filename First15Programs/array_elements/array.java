48d0d686a2fa2aeedbfb68555faebb6d7329aca5
sharbinaashraf07 <79473028+sharbinaashraf07@users.noreply.github.com>
2021-09-14 13:57:34 +0530

    commit

---
+++ First15Programs/array_elements/array.java
@@ -0,0 +1,9 @@
+public class array{
+public static void main(String args[]){
+int[] num={12,4,5,2};
+System.out.println("First element:"+num[0]);
+System.out.println("Second element:"+num[1]);
+System.out.println("Third element:"+num[2]);
+System.out.println("Fourth element:"+num[3]);
+}
+}
---
+++ First15Programs/array_for_each/arrayfor.java
@@ -0,0 +1,9 @@
+public class arrayfor{
+public static void main(String args[]){
+String[] flower={"lilly","jasmine","rose","hibiscus","dahlia"};
+for(String i:flower)
+{
+System.out.println(i);
+}
+}
+}
